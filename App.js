4a18a2f6b4921dd7be37ec4308204e7f8734dd53
OZhang <zhangrunze@163.com>
2017-10-12 23:14:27 +0800

    init

---
+++ App.js
@@ -0,0 +1,104 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import signalr from 'react-native-signalr';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+  'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+  componentDidMount() {
+    //This is the server under /example/server published on azure.
+    const connection = signalr.hubConnection('http://react-native-signalr20171012104248.azurewebsites.net');
+    connection.logging = true;
+
+    const proxy = connection.createHubProxy('chatHub');
+    //receives broadcast messages from a hub function, called "helloApp"
+    proxy.on('helloApp', (argOne, argTwo, argThree, argFour) => {
+      console.log('message-from-server', argOne, argTwo, argThree, argFour);
+      //Here I could response by calling something else on the server...
+    });
+
+    // atempt connection, and handle errors
+    connection.start().done(() => {
+      console.log('Now connected, connection ID=' + connection.id);
+
+      proxy.invoke('helloServer', 'Hello Server, how are you?')
+        .done((directResponse) => {
+          console.log('direct-response-from-server', directResponse);
+        }).fail(() => {
+          console.warn('Something went wrong when calling server, it might not be up and running?')
+        });
+
+    }).fail(() => {
+      console.log('Failed');
+    });
+
+    //connection-handling
+    connection.connectionSlow(() => {
+      console.log('We are currently experiencing difficulties with the connection.')
+    });
+
+    connection.error((error) => {
+      const errorMessage = error.message;
+      let detailedError = '';
+      if (error.source && error.source._response) {
+        detailedError = error.source._response;
+      }
+      if (detailedError === 'An SSL error has occurred and a secure connection to the server cannot be made.') {
+        console.log('When using react-native-signalr on ios with http remember to enable http in App Transport Security https://github.com/olofd/react-native-signalr/issues/14')
+      }
+      console.log('SignalR error: ' + errorMessage, detailedError)
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
